56396a96d17587c5fed5e3964a27049a839245bd
Benoît Claveau <benoit.claveau@gmail.com>
2017-08-02 17:18:24 +0200

    remove theme: hand

--- sequence-diagram-loader.js
+++ sequence-diagram-loader.js
@@ -16,7 +16,7 @@ sequence.onload = function() {
 	var div = document.createElement("div")
 	div.id = "diagram";
 	document.body.appendChild(div);
-	Diagram.parse(code.innerText).drawSVG("diagram", {theme: 'hand'});
+	Diagram.parse(code.innerText).drawSVG("diagram", {});
 }
 document.body.appendChild(font);
 document.body.appendChild(raphael);
